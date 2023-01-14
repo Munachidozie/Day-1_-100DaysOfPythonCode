493383b469f409876af53f1b71b06f284cd01116
Munachimso Ukaoha <65025073+Munachidozie@users.noreply.github.com>
2023-01-14 21:33:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print('''My name is Munachi
+Today is 12th of January 2022
+I am signing up for Replit's 100 days of Python challenge
+I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+I can say I feel ready 😮‍💨
+You can follow my progress at replit.com/@munachidozie''')
